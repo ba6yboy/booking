009dd9e03e00b66a8dde30bfa56f159b391de045
ba6yboy <ch.andrey.14@gmail.com>
2026-06-07 16:54:00 +0300

    Initial commit: Project setup

---
+++ src/main/java/org/example/bookingsys/BookingSysApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bookingsys/BookingSysApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookingsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
